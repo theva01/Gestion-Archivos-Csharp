a0abfef491a5d31c3e7fc89a116433e26a00efb5
theva01 <Oeduardo263@gmail.com>
2024-04-06 13:52:16 -0500

    Leer y escribir en arvhivos StreamWriter y StreamReader

--- GestionArchivosCsharp/Form1.Designer.cs
+++ GestionArchivosCsharp/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEscribir = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEscribir
+            // 
+            this.btnEscribir.Location = new System.Drawing.Point(284, 86);
+            this.btnEscribir.Name = "btnEscribir";
+            this.btnEscribir.Size = new System.Drawing.Size(227, 108);
+            this.btnEscribir.TabIndex = 0;
+            this.btnEscribir.Text = "Escribir";
+            this.btnEscribir.UseVisualStyleBackColor = true;
+            this.btnEscribir.Click += new System.EventHandler(this.btnEscribir_Click);
+            // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(284, 282);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(227, 96);
+            this.btnLeer.TabIndex = 1;
+            this.btnLeer.Text = "Leer";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLeer);
+            this.Controls.Add(this.btnEscribir);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEscribir;
+        private System.Windows.Forms.Button btnLeer;
     }
 }
 
--- GestionArchivosCsharp/Form1.cs
+++ GestionArchivosCsharp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GestionArchivosCsharp
 {
@@ -15,6 +16,24 @@ namespace GestionArchivosCsharp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnEscribir_Click(object sender, EventArgs e)
+        {
+            TextWriter escribe = new StreamWriter("Test.txt");
+            escribe.WriteLine("Hola PC MAster!");
+            escribe.Close();
+            StreamWriter agregar = File.AppendText("Test.txt");
+            agregar.WriteLine("Hola agrego mas lineas");
+            agregar.Close();
+            MessageBox.Show("Listo");
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            TextReader leer = new StreamReader("Test.txt");
+            MessageBox.Show(leer.ReadToEnd());
+            leer.Close();
         }
     }
 }
