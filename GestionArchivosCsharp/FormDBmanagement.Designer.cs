a0a36629ddfcd8b2515b1ac68992f8b22b523896
theva01 <Oeduardo263@gmail.com>
2024-04-20 05:35:57 -0500

    Agregando actualizar y opitimizando los metodos

--- GestionArchivosCsharp/FormDBmanagement.Designer.cs
+++ GestionArchivosCsharp/FormDBmanagement.Designer.cs
@@ -28,13 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnConsulta = new System.Windows.Forms.Button();
             this.txtConsulta = new System.Windows.Forms.TextBox();
             this.LblPais = new System.Windows.Forms.Label();
             this.txtId = new System.Windows.Forms.TextBox();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.lblNombre = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
@@ -43,22 +41,26 @@
             this.label4 = new System.Windows.Forms.Label();
             this.txtPais = new System.Windows.Forms.TextBox();
             this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtBuscarId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(64, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(21, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(724, 387);
+            this.dataGridView1.Size = new System.Drawing.Size(724, 497);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnConsulta
             // 
-            this.btnConsulta.Location = new System.Drawing.Point(794, 401);
+            this.btnConsulta.Location = new System.Drawing.Point(810, 397);
             this.btnConsulta.Name = "btnConsulta";
-            this.btnConsulta.Size = new System.Drawing.Size(92, 34);
+            this.btnConsulta.Size = new System.Drawing.Size(253, 34);
             this.btnConsulta.TabIndex = 1;
             this.btnConsulta.Text = "CONSULTAR";
             this.btnConsulta.UseVisualStyleBackColor = true;
@@ -66,15 +68,15 @@
             // 
             // txtConsulta
             // 
-            this.txtConsulta.Location = new System.Drawing.Point(64, 409);
+            this.txtConsulta.Location = new System.Drawing.Point(837, 444);
             this.txtConsulta.Name = "txtConsulta";
-            this.txtConsulta.Size = new System.Drawing.Size(724, 20);
+            this.txtConsulta.Size = new System.Drawing.Size(100, 20);
             this.txtConsulta.TabIndex = 2;
             // 
             // LblPais
             // 
             this.LblPais.AutoSize = true;
-            this.LblPais.Location = new System.Drawing.Point(21, 412);
+            this.LblPais.Location = new System.Drawing.Point(804, 447);
             this.LblPais.Name = "LblPais";
             this.LblPais.Size = new System.Drawing.Size(27, 13);
             this.LblPais.TabIndex = 3;
@@ -86,11 +88,6 @@
             this.txtId.Name = "txtId";
             this.txtId.Size = new System.Drawing.Size(100, 20);
             this.txtId.TabIndex = 4;
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // lblNombre
             // 
@@ -114,7 +111,7 @@
             // 
             this.txtNombre.Location = new System.Drawing.Point(837, 94);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.Size = new System.Drawing.Size(226, 20);
             this.txtNombre.TabIndex = 7;
             // 
             // label3
@@ -154,17 +151,57 @@
             // 
             this.btnAgregar.Location = new System.Drawing.Point(810, 239);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(127, 31);
+            this.btnAgregar.Size = new System.Drawing.Size(253, 31);
             this.btnAgregar.TabIndex = 13;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(810, 288);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(127, 31);
+            this.btnActualizar.TabIndex = 14;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(810, 336);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(253, 31);
+            this.btnEliminar.TabIndex = 15;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtBuscarId
+            // 
+            this.txtBuscarId.Location = new System.Drawing.Point(952, 294);
+            this.txtBuscarId.Name = "txtBuscarId";
+            this.txtBuscarId.Size = new System.Drawing.Size(118, 20);
+            this.txtBuscarId.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(949, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Buscar ID";
+            // 
             // FormDBmanagement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(970, 453);
+            this.ClientSize = new System.Drawing.Size(1082, 531);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBuscarId);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtPais);
@@ -179,7 +216,7 @@
             this.Controls.Add(this.btnConsulta);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FormDBmanagement";
-            this.Text = "FormDBmanagement";
+            this.Text = "Gestion de Personas";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -193,7 +230,6 @@
         private System.Windows.Forms.TextBox txtConsulta;
         private System.Windows.Forms.Label LblPais;
         private System.Windows.Forms.TextBox txtId;
-        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Label lblNombre;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtNombre;
@@ -202,5 +238,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox txtPais;
         private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtBuscarId;
+        private System.Windows.Forms.Label label1;
     }
 }
--- GestionArchivosCsharp/FormDBmanagement.cs
+++ GestionArchivosCsharp/FormDBmanagement.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Collections;
 
 namespace GestionArchivosCsharp
 {
@@ -21,7 +22,28 @@ namespace GestionArchivosCsharp
             InitializeComponent();
             
         }
+        private void mensaje(string msm) { MessageBox.Show(msm); }
+        private void actualizaTabla(string query) {
+            SqlCommand consulta = new SqlCommand(query, conexion);
+            SqlDataAdapter data = new SqlDataAdapter(consulta);
+            DataTable table = new DataTable();
+            data.Fill(table);
+            dataGridView1.DataSource = table;
+        }
 
+        private void limpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtEdad.Text = "";
+            txtPais.Text = "";
+            txtId.Text = "";
+        }
+        
+        public void consultar()
+        {
+            actualizaTabla("Select * from Persona");
+        }
+       
         private void btnConsulta_Click(object sender, EventArgs e)
         {
             string pais = txtConsulta.Text;
@@ -34,53 +56,105 @@ namespace GestionArchivosCsharp
             {
                 try
                 {
-                    string query = $"SELECT * FROM Persona WHERE Pais = '{pais}'";
-                    SqlCommand consulta = new SqlCommand(query, conexion);
-                    SqlDataAdapter data = new SqlDataAdapter(consulta);
-                    DataTable table = new DataTable();
-                    data.Fill(table);
-                    dataGridView1.DataSource = table;
+                    actualizaTabla($"SELECT * FROM Persona WHERE Pais = '{pais}'");
                 }
                 catch (Exception exe)
                 {
 
-                    MessageBox.Show(exe.ToString());
+                    mensaje(exe.ToString());
                 }
                 
             }
         }
-
-        public void consultar()
+        
+        private int opcionQuery(string query)
         {
-            string query = "Select * from Persona";
-            SqlCommand consulta = new SqlCommand(query, conexion);
-            SqlDataAdapter data = new SqlDataAdapter(consulta);
-            DataTable table = new DataTable();
-            data.Fill(table);
-            dataGridView1.DataSource = table;
-        }
-        private void btnAgregar_Click(object sender, EventArgs e)
-        {
-
             if (conexion.State == ConnectionState.Closed)
             {
                 conexion.Open();
             }
-            string cadenaQuery = "INSERT INTO Persona (Nombre, Edad, Pais, id)" +
-                $" VALUES ('{txtNombre.Text}', '{txtEdad.Text}', '{txtPais.Text}', '{txtId.Text}')";
+            string cadenaQuery = query;
             SqlCommand sqlConector = new SqlCommand(cadenaQuery, conexion);
-            sqlConector.ExecuteNonQuery();//ejecuta el query
+            try
+            {
+                return sqlConector.ExecuteNonQuery();//ejecuta el query
+            }
+            catch (Exception)
+            {
+                return 2;
+            }
+            
+        }
 
-            MessageBox.Show($"La persona: {txtNombre.Text} se agrego correctamente");
+        private void crUD(string query, string UD)
+        {
+            int flag = opcionQuery(query);
 
-            txtNombre.Text = "";
-            txtEdad.Text = "";
-            txtPais.Text = "";
-            txtId.Text = "";
+            switch (flag)
+            {
+                case 0:
+                    mensaje($"No hay registro que coinsida para {UD}");
+                    
+                    break;
 
+                case 1:
+                    if (UD == "eliminar")
+                    {
+                        mensaje("Se elemino el registro correctamente");
+                    }
+                    else if (UD == "actualizar")
+                    {
+                        mensaje("Se actualizo el registro correctamente");
+                    }
+                    consultar();
+                    break;
 
-            consultar();
+                 case 2:
+                    mensaje(UD);
+                    break;
 
+                default:
+                    mensaje("Opción no válida");
+                    break;
+            }
+            limpiarCampos();
+            
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "" && txtEdad.Text != "" && txtPais.Text != "" && txtId.Text != "")
+            {
+
+                if (!int.TryParse(txtEdad.Text, out int edad))
+                {
+                    mensaje("La edad debe ser un número entero válido.");
+                    return;
+                }
+
+                if (edad <= 0)
+                {
+                    mensaje("La edad debe ser mayor que cero.");
+                    return;
+                }
+                crUD("INSERT INTO Persona (Nombre, Edad, Pais, id)" +
+                $" VALUES ('{txtNombre.Text}', '{txtEdad.Text}', '{txtPais.Text}', '{txtId.Text}')", $"La persona: {txtNombre.Text} se agrego correctamente");
+            }
+            else
+            {
+                mensaje("Todos los campos son obligatorios.");
+            }
+            
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            crUD($"DELETE FROM Persona WHERE Nombre = '{txtNombre.Text}'", "eliminar");
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            crUD($"UPDATE Persona SET Nombre = '{txtNombre.Text}', Edad = '{txtEdad.Text}', Pais = '{txtPais.Text}', id = '{txtId.Text}' WHERE id = '{txtBuscarId.Text}'", "actualizar");
         }
     }
 }
